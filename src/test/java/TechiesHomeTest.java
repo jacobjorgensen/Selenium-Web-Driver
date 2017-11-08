25cf18f0b74bbbd9135c7a491c905acc2f577c11
Jacob Laqua <jlaqua@mail.greenriver.edu>
2017-11-07 19:46:09 -0800

    Implemented a passing test and failing test. These tests search for id attributes on the navigational links of the home page

--- src/test/java/TechiesHomeTest.java
+++ src/test/java/TechiesHomeTest.java
@@ -1,5 +1,4 @@
 import junit.framework.TestCase;
-import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,21 +6,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TechiesHomeTest extends TestCase {
 
     private WebDriver chromeDriver;
-    @Before
-    public void start()
+
+    /**
+     * This method points to the chromedriver.exe file,
+     * instantiates a new WebDriver object then opens the home page
+     * for the site.
+     */
+    public void startHomePage()
     {
         System.setProperty("webdriver.chrome.driver", "files/chromedriver.exe");
-        //Start
         chromeDriver = new ChromeDriver();
-
         chromeDriver.get("http://techies.greenrivertech.net/");
     }
 
-    public void testSiteConnection()
+    /**
+     * This method texts the showing text for the
+     * About link.
+     */
+    public void testAboutLinkText()
     {
-        start();
-
-        //compare title
+        startHomePage();
         assertEquals("About", chromeDriver.findElement(By.id("about-link")).getText());
+    }
+
+    /**
+     * This method tests the showing text fro the
+     * Contact link.
+     *
+     * Note - This method is intended to fail because the id being searched
+     * for doesn't exist
+     */
+    public void testContactLinkText()
+    {
+        startHomePage();
+        assertEquals("Contact", chromeDriver.findElement(By.id("contact-link")).getText());
     }
 }
